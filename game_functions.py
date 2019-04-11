7ecb1eed65a878f76d9cea78ca7a38c350142903
johnsoncor <johnsoncor@cwu.edu>
2019-04-10 21:25:41 -0700

    Added game active state.

--- game_functions.py
+++ game_functions.py
@@ -110,6 +110,15 @@ def create_fleet(ai_settings, screen, ship, aliens):
             # Create an alien and place it in the row.
             create_alien(ai_settings, screen, aliens, alien_number, row_number)
 
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """Check if any aliens have reached the bottom of the screen."""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            # Treat this the same as if the ship got hit.
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
+
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     """Check if the fleet is at an edge, and then update the
     positions of all aliens ini the fleet."""
@@ -119,6 +128,9 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     # Look for alien-ship collisions.
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+
+    # Look for aliens hitting the bottom of the screen.
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
 
 def check_fleet_edges(ai_settings, aliens):
     """Respond appropriately if any aliens have reached an edge."""
@@ -135,16 +147,19 @@ def change_fleet_direction(ai_settings, aliens):
 
 def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     """Respond to ship being hit by alien."""
-    # Decrement number of ships left.
-    stats.ships_left -= 1
+    if stats.ships_left > 0:
+        # Decrement number of ships left.
+        stats.ships_left -= 1
 
-    # Empty the list of aliens and bullets.
-    aliens.empty()
-    bullets.empty()
+        # Empty the list of aliens and bullets.
+        aliens.empty()
+        bullets.empty()
 
-    # Create a new fleet and center the ship.
-    create_fleet(ai_settings, screen, ship, aliens)
-    ship.center_ship()
+        # Create a new fleet and center the ship.
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
 
-    # Pause game.
-    sleep(0.5)
+        # Pause game.
+        sleep(0.5)
+    else:
+        stats.game_active = False
